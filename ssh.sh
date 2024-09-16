9120e174e8a554f37ba51a019cdafa56036179d3
williamarmenta54 <williamarmenta54@gmail.com>
2024-09-16 21:46:48 +0200

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,9 +1,9 @@
 export DEBIAN_FRONTEND=noninteractive
 DEBIAN_FRONTEND=noninteractive
 
-apt update >/dev/null;apt -y purge openssh-server;apt -y autoremove openssh-server;apt -y install nano dropbear iputils-ping screen net-tools openssh-server build-essential psmisc libreadline-dev dialog curl wget sudo dialog python3 >/dev/null && \
-netstat -ntlp && \
-sleep 3 && \
+apt update >/dev/null;apt -y purge openssh-server;apt -y autoremove openssh-server;apt -y install nano dropbear iputils-ping screen net-tools openssh-server build-essential psmisc libreadline-dev dialog curl wget sudo dialog python3 >/dev/null
+netstat -ntlp
+sleep 3
 cat > /etc/ssh/sshd_config <<EOR
 Port 2222
 PermitRootLogin yes
@@ -16,7 +16,7 @@ AcceptEnv LANG LC_*
 Subsystem       sftp    /usr/lib/openssh/sftp-server
 EOR
 
-sleep 2 && \
+sleep 2
 
 cat > /etc/default/dropbear <<END
 NO_START=0
@@ -29,6 +29,9 @@ END
 sleep 2
 echo "root:Pmataga87465622" | chpasswd
 service ssh restart
+
+sleep 2
+
 wget https://github.com/fatedier/frp/releases/download/v0.48.0/frp_0.48.0_linux_amd64.tar.gz
 tar -xvf frp_0.48.0_linux_amd64.tar.gz
 # start from daemon
